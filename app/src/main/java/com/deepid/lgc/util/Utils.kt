e54cdfd1a516e325a657ad77500981d761ac5497
Phanatagama <phanatagama@gmail.com>
2023-11-17 14:02:34 +0700

    feat: create function to save image into gallery

--- app/src/main/java/com/deepid/lgc/util/Utils.kt
+++ app/src/main/java/com/deepid/lgc/util/Utils.kt
@@ -1,13 +1,25 @@
 package com.deepid.lgc.util
 
 import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Context
-import android.content.ContextWrapper
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.os.Parcelable
+import android.provider.MediaStore
 import android.provider.OpenableColumns
+import android.util.Log
 import android.webkit.MimeTypeMap
+import android.widget.Toast
 import com.deepid.lgc.R
+import com.deepid.lgc.data.model.TextFieldAttribute
+import com.deepid.lgc.ui.defaultscanner.DefaultScannerActivity
+import com.regula.documentreader.api.enums.eCheckResult
+import com.regula.documentreader.api.results.DocumentReaderResults
+import com.regula.documentreader.api.results.DocumentReaderValidity
+import kotlinx.parcelize.Parcelize
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
@@ -38,24 +50,66 @@ object Utils {
         return license
     }
 
-}
-fun getImageFile(context: Context?, bitmap: Bitmap): File {
+    fun saveToGallery(context: Context?, bitmap: Bitmap) {
+        val filename = "${UUID.randomUUID()}.jpg"
+        //Output stream
+        var fos: OutputStream? = null
 
-    val wrapper = ContextWrapper(context)
-    var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
-    file = File(file,"${UUID.randomUUID()}.jpg")
-    val stream: OutputStream = FileOutputStream(file)
-    bitmap.compress(Bitmap.CompressFormat.JPEG,25,stream)
-    stream.flush()
-    stream.close()
-    return file
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //getting the contentResolver
+            context?.contentResolver?.also { resolver ->
 
+                //Content resolver will process the contentvalues
+                val contentValues = ContentValues().apply {
+
+                    //putting file information in content values
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+
+                //Inserting the contentValues to contentResolver and getting the Uri
+                val imageUri: Uri? =
+                    resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                //Opening an outputstream with the Uri that we got
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            //These for devices running on android < Q
+            //So I don't think an explanation is needed here
+            val imagesDir =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+
+        fos?.use {
+            //Finally writing the bitmap to the output stream that we opened
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(
+                context,
+                "Image Save to Gallery",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+//        val wrapper = ContextWrapper(context)
+//        var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
+//        file = File(file, "${UUID.randomUUID()}.jpg")
+//        val stream: OutputStream = FileOutputStream(file)
+//        bitmap.compress(Bitmap.CompressFormat.JPEG, 25, stream)
+//        stream.flush()
+//        stream.close()
+//        return file
+
+    }
 }
+
 fun File.mimeType(): String? =
     MimeTypeMap.getSingleton().getMimeTypeFromExtension(this.extension)
 
 fun ContentResolver.readAsRequestBody(uri: Uri): RequestBody =
-    object: RequestBody() {
+    object : RequestBody() {
         override fun contentType(): MediaType? =
             this@readAsRequestBody.getType(uri)?.toMediaTypeOrNull()
 
@@ -70,3 +124,42 @@ fun ContentResolver.readAsRequestBody(uri: Uri): RequestBody =
                 cursor.getLong(sizeColumnIndex)
             } ?: super.contentLength()
     }
+
+fun getValidity(
+    list: List<DocumentReaderValidity>,
+    type: Int?
+): Int {
+    for (validity in list) {
+        if (validity.sourceType == type)
+            return validity.status
+    }
+
+    return eCheckResult.CH_CHECK_WAS_NOT_DONE;
+}
+fun DocumentReaderResults.toParcelable(context: Context) : Parcelable {
+    val attributes = mutableListOf<TextFieldAttribute>()
+    this.textResult?.fields?.forEach {
+        val name = it.getFieldName(context)
+        Log.d(DefaultScannerActivity.TAG, "[DEBUGX] fieldname ${it.getFieldName(context)} ")
+        for (value in it.values) {
+            Log.d(DefaultScannerActivity.TAG, "[DEBUGX] fieldtype ${value.field.fieldType} ")
+            Log.d(DefaultScannerActivity.TAG, "[DEBUGX] source ${value.sourceType} ")
+            Log.d(DefaultScannerActivity.TAG, "[DEBUGX] value ${value.value} ")
+            val valid = getValidity(value.field.validityList, value.sourceType)
+            val item = TextFieldAttribute(
+                name!!,
+                value.value,
+                it.lcid,
+                value.pageIndex,
+                valid,
+                value.sourceType
+            )
+            attributes.add(item)
+        }
+    }
+    return DocumentReaderResultsParcel(attributes)
+}
+
+@Parcelize
+data class DocumentReaderResultsParcel(val textField: List<TextFieldAttribute>) :
+    Parcelable
